11cc0f60b3bb52760312c7cc5565bb67e848f87a
samarkun <samarkun4@gmail.com>
2024-09-26 16:36:36 +0530

    new cart button function add

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick =  () =>{
     navbar.classList.toggle('active');
 } 
+
+
+
+
+const btn = document.querySelector("#cart-btn");
+const container = document.querySelector(".cart-item-container");
+btn.addEventListener('click', () => {
+    const container = document.querySelector(".cart-item-container");
+   // container.style.margin = '165.2vh';
+   // container.style.backgroundColor = 'white';
+   // container.style.setProperty('height', '200px');
+    container.style.setProperty('margin', '21.3vh 0px 0px 165.2vh');
+    alert("hy");
+})
